506d26d7f08d6e21876d809314b60438a7279447
Erik Hollensbe <erik@hollensbe.org>
2010-05-25 01:03:17 -0400

    * Forgot to commit the mock driver.

---
+++ lib/driver/mock.rb
@@ -0,0 +1,20 @@
+module RDBI
+    module Driver
+        class Mock
+            class DBH < RDBI::Handle
+                def ping
+                    10
+                end
+            end
+
+            def initialize(*args)
+                # FIXME emacros
+                @hash = args[0]
+            end
+
+            def get_handle
+                return DBH.new 
+            end
+        end
+    end
+end

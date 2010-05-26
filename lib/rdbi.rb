38385cc2dc7c584f9285b29be37ddc711bbd31c2
Erik Hollensbe <erik@hollensbe.org>
2010-05-26 08:14:45 -0400

    * Docs for RDBI and a few spots in RDBI::Pool. * 100% test coverage.

--- lib/rdbi.rb
+++ lib/rdbi.rb
@@ -6,11 +6,33 @@ module RDBI
     # FIXME would like to use methlab here, but am not entirely sure how to do this best.
     #
     class << self
+        #
+        # Every database handle allocated throughout the lifetime of the
+        # program. This functionality is subject to change and may be pruned
+        # during disconnection.
         attr_reader :all_connections
-        attr_reader :drivers
+        #--
+        #attr_reader :drivers
+        #++
+        #
+        # The last database handle allocated. This may come from pooled connections or regular ones.
         attr_reader :last_dbh
     end
 
+    #
+    # connect() takes a class name, which may be represented as:
+    #
+    # * The full class name, such as RDBI::Driver::Mock
+    # * A symbol representing the significant portion, such as :Mock, which corresponds to RDBI::Driver::Mock
+    # * A string representing the same data as the symbol.
+    #
+    # Additionally, arguments that are passed on to the driver for consumption
+    # may be passed. Please refer to the driver documentation for more
+    # information.
+    #
+    # connect() returns an instance of RDBI::Database. In the instance a block
+    # is provided, it will be called upon connection success, with the
+    # RDBI::Database object provided in as the first argument.
     def self.connect(klass, *args)
         klass = case klass
                 when Class
@@ -33,6 +55,16 @@ module RDBI
         return dbh
     end
 
+    #
+    # connect_cached() works similarly to connect, but yields a database handle
+    # copied from a RDBI::Pool. The 'default' pool is the ... default, but this
+    # may be manipulated by providing :pool_name to the connection arguments.
+    #
+    # If a pool does not exist already, it will be created and a database
+    # handle instanced from your connection arguments.
+    #
+    # If a pool *already* exists, your connection arguments will be ignored and
+    # it will instance from the Pool's connection arguments.
     def self.connect_cached(klass, *args, &block)
         args = args[0]
         pool_name = args[:pool_name] || :default
@@ -51,18 +83,26 @@ module RDBI
         return dbh
     end
 
+    #
+    # Retrieves a RDBI::Pool. See RDBI::Pool.[].
     def self.pool(pool_name=:default)
         RDBI::Pool[pool_name]
     end
 
+    #
+    # Connects to and pings the database. Arguments are the same as for RDBI.connect.
     def self.ping(klass, *args)
         connect(klass, *args).ping
     end
-    
+   
+    #
+    # Reconnects all known connections. See RDBI.all_connections.
     def self.reconnect_all
         @all_connections.each(&:reconnect)
     end
-    
+   
+    #
+    # Disconnects all known connections. See RDBI.all_connections.
     def self.disconnect_all
         @all_connections.each(&:disconnect)
     end
@@ -84,20 +124,28 @@ end
 #
 class RDBI::Pool
     class << self
+        #
+        # Retrieves a pool object for the name, or nothing if it does not exist.
         def [](name)
             @pools ||= { }
             @pools[name.to_sym]
         end
 
+        #
+        # Sets the pool for the name. This is not recommended for end-user code.
         def []=(name, value)
             @pools ||= { }
             @pools[name.to_sym] = value
         end
     end
 
+    # a list of the pool handles for this object. Do not manipulate this directly.
     attr_reader :handles
+    # the last index corresponding to the latest allocation request.
     attr_reader :last_index
+    # the maximum number of items this pool can hold. should only be altered by resize.
     attr_reader :max
+    # the Mutex for this pool.
     attr_reader :mutex
 
     #

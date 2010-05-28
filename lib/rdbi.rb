39ecb068ee0932f810b65ee22a38465c3437174e
Erik Hollensbe <erik@hollensbe.org>
2010-05-28 05:47:19 -0400

    * Whoops, forgot to gut the original Driver class. Ugh.

--- lib/rdbi.rb
+++ lib/rdbi.rb
@@ -105,22 +105,6 @@ module RDBI
     end
 end
 
-class RDBI::Driver
-    attr_reader :connect_args
-    attr_reader :dbh_class
-
-    def initialize(dbh_class, *args)
-        @dbh_class = dbh_class
-        @connect_args = args
-    end
-
-    def new_handle 
-        dbh = @dbh_class.new(*@connect_args)
-        dbh.driver = self.class
-        return dbh
-    end
-end
-
 require 'rdbi/pool'
 require 'rdbi/driver'
 require 'rdbi/database'

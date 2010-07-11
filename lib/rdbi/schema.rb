ab4604988fb2c6fdfcc9847de3946da24df30e93
Erik Hollensbe <erik@hollensbe.org>
2010-07-11 02:46:00 -0400

    * Stub docs for RDBI::Schema and RDBI::Column. * Had to, er, "coerce" rdoc to document these in not the best way.

--- lib/rdbi/schema.rb
+++ lib/rdbi/schema.rb
@@ -18,4 +18,81 @@ RDBI::Column = Struct.new(
   :table
 )
 
+#
+# RDBI::Schema is the metadata representation of a single schema "object", such
+# as a single table or the data queried against during RDBI::Statement
+# execution.
+#
+# FIXME stub docs -- elaborate
+#
+class RDBI::Schema
+  ##
+  # :attr_reader: columns
+  #
+  # FIXME
+
+  ##
+  # :attr_reader: tables
+  #
+  # FIXME
+
+  ##
+  # :attr_reader: type 
+  #
+  # FIXME
+end
+
+#
+# RDBI::Column is the metadata representation of a single table column. You
+# will typically access this via RDBI::Schema.
+#
+# FIXME stub docs -- elaborate
+#
+class RDBI::Column
+  ##
+  # :attr_reader: name 
+  #
+  # FIXME
+  
+  ##
+  # :attr_reader: type
+  #
+  # FIXME
+  
+  ##
+  # :attr_reader: ruby_type
+  #
+  # FIXME
+  
+  ##
+  # :attr_reader: precision
+  #
+  # FIXME
+ 
+  ##
+  # :attr_reader: scale
+  #
+  # FIXME
+  
+  ##
+  # :attr_reader: nullable
+  #
+  # FIXME
+  
+  ##
+  # :attr_reader: metadata 
+  #
+  # FIXME
+
+  ##
+  # :attr_reader: default
+  #
+  # FIXME
+  
+  ##
+  # :attr_reader: table
+  #
+  # FIXME
+end
+
 # vim: syntax=ruby ts=2 et sw=2 sts=2

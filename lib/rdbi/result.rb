4bbf80112519dae890cc90f322c763130291cb8b
Erik Hollensbe <erik@hollensbe.org>
2010-07-11 01:41:19 -0400

    * RDBI::Result docs.

--- lib/rdbi/result.rb
+++ lib/rdbi/result.rb
@@ -1,22 +1,105 @@
+#
+# RDBI::Result encapsulates results from a statement.
+#
+# Results in RDBI::Result are row-oriented and may be transformable by Result
+# Drivers (RDBI::Result::Driver). They are fetched as a unit or in order.
+#
+# The RDBI::Result API is deliberately architected to loosely resemble that of
+# IO or File.
+#
+# == Just give me the data!
+#
+# Have a peek at fetch.
+#
+# == Result Counts
+#
+# Multiple kinds of counts are represented in each result:
+#
+# * A count of the results provided
+# * A count of the affected rows.
+#
+# To elaborate, the "affected rows" is a count of rows that were altered by the
+# statement from a DML result such as +INSERT+ or +UPDATE+. In some cases,
+# statements will both alter rows and yield results, which is why this value is
+# not multiplexed. 
+#
+# == Result Drivers
+#
+# Result drivers are subclasses of RDBI::Result::Driver that take the result as
+# input and yield a transformed input: data structures such a hashes, or even
+# wilder results such as CSV or JSON or YAML. Given the ability to sanely
+# transform row-oriented input, result driver effectively have the power to do
+# anything.
+#
+# Accessing result drivers is as easy as using a secondary form of
+# RDBI::Result#fetch or more explicitly with the RDBI::Result#as call.
+#
 class RDBI::Result
   extend MethLab
   include Enumerable
 
+  # The RDBI::Schema structure associated with this result.
   attr_reader :schema
+
+  # The RDBI::Statement that yielded this result.
   attr_reader :sth
+
+  # The RDBI::Result::Driver currently associated with this Result.
   attr_reader :driver
+
+  # The count of results (see RDBI::Result main documentation)
   attr_reader :result_count
+
+  # The count of affected rows by a DML statement (see RDBI::Result main documentation)
   attr_reader :affected_count
+
+  # The mapping of types for each positional argument in the Result.
   attr_reader :type_hash
+
+  # The binds used in the statement that yielded this Result.
   attr_reader :binds
 
   # FIXME async
   inline(:complete, :complete?) { true }
 
+  ##
+  # :attr_reader: eof
+  #
+  # Are we at the end of the results?
+  
+  ##
+  # :attr_reader: eof?
+  #
+  # Are we at the end of the results?
   inline(:eof, :eof?)           { @index >= @data.size }
+
+  ##
+  # :attr_reader: more
+  #
+  # Do we have more input available?
+  
+  ##
+  # :attr_reader: more?
+  #
+  # Do we have more input available?
   inline(:more, :more?)         { @index  < @data.size }
+
+  ##
+  # :attr_reader: has_data
+  #
+  # Does this result have data?
+  
+  ##
+  # :attr_reader: has_data?
+  #
+  # Does this result have data?
   inline(:has_data, :has_data?) { @data.size > 0 }
 
+  #
+  # Creates a new RDBI::Result. Please refer to RDBI::Statement#new_execution
+  # for instructions on how this is typically use and the contents passed to
+  # the constructor.
+  #
   def initialize(sth, binds, data, schema, type_hash, affected_count=0)
     @schema         = schema
     @data           = data
@@ -32,6 +115,12 @@ class RDBI::Result
     as(@driver)
   end
 
+  #
+  # Reload the result. This will:
+  #
+  # * Execute the statement that yielded this result again, with the original binds 
+  # * Replace the results and other attributes with the new results.
+  #
   def reload
     res = @sth.execute(*@binds)
     @data           = res.instance_variable_get(:@data)
@@ -42,14 +131,42 @@ class RDBI::Result
     @index          = 0
   end
 
+  #
+  # Iterator for Enumerable methods. Yields a row at a time.
+  #
   def each
     yield(fetch[0]) while more?
   end
 
+  #
+  # Reset the index.
+  #
   def rewind
     @index = 0
   end
 
+  #
+  # :call-seq: 
+  #   as(String)
+  #   as(Symbol)
+  #   as(Class)
+  #   as([Class, String, or Symbol], *driver_arguments)
+  # 
+  # Replace the Result Driver. See RDBI::Result's main docs and
+  # RDBI::Result::Driver for more information on Result Drivers.
+  #
+  # You may pass:
+  #
+  # * A Symbol or String which is shorthand for loading from the
+  #   RDBI::Result::Driver namespace -- for example: "CSV" will result in the
+  #   class RDBI::Result::Driver::CSV.
+  # * A full class name.
+  #
+  # There are no naming requirements; the String/Symbol form is just shorthand
+  # for convention.
+  #
+  # Any additional arguments will be passed to the driver's constructor.
+  #
   def as(driver_klass, *args)
 
     driver_klass  = RDBI::Util.class_from_class_or_symbol(driver_klass, RDBI::Result::Driver)
@@ -58,6 +175,42 @@ class RDBI::Result
     @fetch_handle = driver_klass.new(self, *args)
   end
 
+  #
+  # :call-seq:
+  #   fetch()
+  #   fetch(Integer)
+  #   fetch(:first)
+  #   fetch(:last)
+  #   fetch(:all)
+  #   fetch(:rest)
+  #   fetch(amount, [Class, String, or Symbol], *driver_arguments)
+  #
+  # fetch is the way people will typically interact with this class. It yields
+  # some or all of the results depending on the arguments given. Additionally,
+  # it can be supplemented with the arguments passed to RDBI::Result#as to
+  # one-off select a result driver.
+  #
+  # The initial argument can be none or one of many options:
+  #
+  # * An Integer n requests n rows from the result and increments the index.
+  # * No argument uses an Integer count of 1.
+  # * :first yields the first row of the result, regardless of the index. 
+  # * :last yields the last row of the result, regardless of the index.
+  # * :all yields the whole set of rows, regardless of the index.
+  # * :rest yields all the items that have not been fetched, determined by the index.
+  #
+  # == The index
+  #
+  # I bet you're wondering what that is now, right? Well, the index is
+  # essentially a running row count that is altered by certain fetch
+  # operations. This makes sequential fetches much simpler.
+  #
+  # Items that do not use the index do not effect it.
+  #
+  # Result Drivers will always rewind the index, as this implicates a "point of
+  # no return" state change. You may always return to the original driver you
+  # were using, but the index position will be lost.
+  #
   def fetch(row_count=1, driver_klass=nil, *args)
     if driver_klass
       as(driver_klass, *args)
@@ -68,13 +221,13 @@ class RDBI::Result
 
   alias read fetch
 
-  # stub docs:
+  # 
+  # raw_fetch is a straight array fetch without driver interaction. If you
+  # think you need this, please still read the fetch documentation as there is
+  # a considerable amount of overlap.
   #
-  # * symbols do not affect the index
-  # * :first and :last yield a single array.
-  # * numbers do
-  # * Marshal.dump(Marshal.load) is the best way to deep clone that I know of.
-  #   I know it's slow. If you have code to speed it up, please contribute.
+  # This is generally used by Result Drivers to transform results.
+  #
   def raw_fetch(row_count)
     final_res = case row_count
                 when :all
@@ -94,6 +247,10 @@ class RDBI::Result
     RDBI::Util.deep_copy(final_res)
   end
 
+  #
+  # This call finishes the result and the RDBI::Statement handle, scheduling
+  # any unpreserved data for garbage collection.
+  #
   def finish
     @sth.finish
     @data   = nil
@@ -111,6 +268,14 @@ class RDBI::Result::Driver
     @result.rewind
   end
 
+
+  # stub docs:
+  #
+  # * symbols do not affect the index
+  # * :first and :last yield a single array.
+  # * numbers do
+  # * Marshal.dump(Marshal.load) is the best way to deep clone that I know of.
+  #   I know it's slow. If you have code to speed it up, please contribute.
   def fetch(row_count)
     (@result.raw_fetch(row_count) || []).enum_for.with_index.map do |item, i|
       case row_count
